f823b9c7a196fd4edbb5cabca3044bbda52b4878
Vishal Hasnani <hasnanivishal@gmail.com>
2019-02-10 14:58:03 +0530

    Updated data table to be able to handle dynamic objects.

---
+++ src/app/datatable/datatable.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PagerService } from '../pager.service';
+
+@Component({
+  selector: 'app-datatable',
+  templateUrl: './datatable.component.html'
+})
+export class DatatableComponent {
+
+  @Input()
+  sampleData : any;
+
+  @Input()
+  itemPerPage : any;
+
+  // pager object
+  pager: any = {};
+
+  // paged items
+  pagedItems: any[];
+
+  // properties name
+  propName: any[];
+
+  currentPage: any;
+  totalItems: any;
+
+
+  constructor(private pagerService:PagerService)
+  {
+    this.currentPage = 1;
+  }
+
+  ngOnInit()
+  {
+    setTimeout(() => {
+      this.setPage(1);
+      this.totalItems = this.sampleData.length;
+    });
+  }
+
+
+  setPage(page: number) {
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.sampleData.length, page, this.itemPerPage);
+
+    // get current page of items
+    this.pagedItems = this.sampleData.slice(this.pager.startIndex, this.pager.endIndex + 1);
+
+    //get object property name
+    this.propName = Object.getOwnPropertyNames(this.sampleData[0]);
+  }
+  
+
+  
+  loadMore() {
+    if (this.currentPage < this.pager.pages.length) {
+      this.currentPage = this.currentPage + 1;
+      this.setPage(this.currentPage);
+    }
+  }
+
+  loadLess() {
+    if (this.currentPage > 1) {
+      this.currentPage = this.currentPage - 1;
+      this.setPage(this.currentPage);
+    }
+  }
+}
