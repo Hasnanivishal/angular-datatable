629bd45dbc15c439ad59e0e9c443f712a3f6b34c
vishal.hasnani.rtpl <vishal.hasnani@ranosys.com>
2019-02-13 16:10:12 +0530

    Updated Style of datatable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularDataTableLibrary';
+  sampleData = [
+    {id: 1, name: "a"},
+    {id: 2, name: "b"},
+    {id: 3, name: "c"},
+    {id: 4, name: "d"},
+    {id: 5, name: "e"},
+    {id: 6, name: "f"},
+    {id: 7, name: "g"},
+    {id: 8, name: "h"}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DatatableComponent } from './datatable/datatable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatatableComponent
   ],
   imports: [
     BrowserModule
